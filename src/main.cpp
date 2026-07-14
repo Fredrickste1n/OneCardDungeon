5de167bdd6ab99dfb26254602a7129be62d83ede
Fredrickste1n <fredricksteintheharlot@gmail.com>
2026-07-14 17:58:43 -0400

    fix main from template

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,20 @@
 #include <raylib.h>
-#include "ball.h"
 
 int main() 
 {
-    const Color darkGreen = {20, 160, 133, 255};
     
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
-    
-    Ball ball;
+    const int screenWidth = 800;
+    const int screenHeight = 600;
     
     InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
     SetTargetFPS(60);
     
     while (!WindowShouldClose())
     {
-        ball.Update();
         
         BeginDrawing();
-            ClearBackground(darkGreen);
-            ball.Draw();
+            ClearBackground(BLACK);
+
         EndDrawing();
     }
     
